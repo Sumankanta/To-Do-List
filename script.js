c0c44c4a3d9ed072ae0644bb021064f5bf1b934c
Sumankanta Padhan <98678991+Sumankanta@users.noreply.github.com>
2024-01-02 07:18:02 +0530

    Add file

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box")
+const listContener = document.getElementById("list-contener")
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContener.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContener.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContener.innerHTML);
+}
+
+
+function showTask(){
+    listContener.innerHTML = localStorage.getItem("data");
+}
+showTask();
